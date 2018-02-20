45f51200c83b9cb377aaf9065a9d828892049852
royzinn <roy.zinn@gmail.com>
2018-02-20 18:08:52 +0200

    update seeds (for local env check)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Genre.destroy_all
+Address.destroy_all
+Review.destroy_all
+Restaurant.destroy_all
+
+genres = []
+['sushi', 'vegeterian', 'italian', 'israeli', 'oriental'].each { |genre_name| genres << Genre.create!(name: genre_name) }
+
+['hubba bubba', 'sushi samba', 'taizu'].each_with_index do |restaurant_name, i|
+  rest = Restaurant.create!(name: restaurant_name, accepts_10bis: [true, false].sample, max_delivery_time: [10, 20, 30, 40].sample, genres: genres.sample(2))
+  rest.addresses << Address.create!(zip_code: "12#{i}", country: 'israel', country_code: 'il', street: "hadekel #{i}", city: 'avihail', addressable: rest)
+  rest.reviews << Review.create!(comment: ['great food', 'dirty as hell'].sample, reviewer_name: 'john doe', reviewable: rest, rating: [1, 2, 3].sample)
+end
+
