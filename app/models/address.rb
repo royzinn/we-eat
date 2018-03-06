f578fe253ac2d7963b13c89ffd93b23c0f7e52e7
royzinn <roy.zinn@gmail.com>
2018-03-06 11:07:19 +0200

    handle geocoding for addresses on save + set some known address in seed

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,13 @@ class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
   validates :zip_code, :city, :street, :country, presence: true
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  private
+
+  def full_address
+    "#{street}, #{city}, #{country}"
+  end
 end
