472f196a90438a0d9d9f874d75c0d7076f577965
royzinn <roy.zinn@gmail.com>
2018-03-12 17:35:49 +0200

    eslint fixes

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.body.appendChild(document.createElement('div')));
