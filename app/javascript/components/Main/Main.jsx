da29acf557331259a2da5ef92c267cf1bcc91cde
royzinn <roy.zinn@gmail.com>
2018-03-13 09:54:47 +0200

    remove leftovers...

--- app/javascript/components/Main/Main.jsx
+++ app/javascript/components/Main/Main.jsx
@@ -3,28 +3,12 @@ import Header from '../Header/Header';
 import FiltersBar from '../FiltersBar/FiltersBar';
 import RestaurantsMain from '../Restaurants/RestaurantsMain/RestaurantsMain';
 
-export default class Main extends Component {
-  filterRating = (e) => {
-    const value = e.target.value.match(/\d+/);
-    const rateFilter = (value && Number(value[0])) || 'SHOW_ALL';
-
-    this.setState(() => ({ rateFilter }));
-  }
-
-  filterDeliveryTime = (e) => {
-    const value = e.target.value.match(/\d+/);
-    const deliveryTimeFilter = (value && Number(value[0])) || 'SHOW_ALL';
-
-    this.setState(() => ({ deliveryTimeFilter }));
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header />
-        <FiltersBar />
-        <RestaurantsMain />
-      </div>
-    );
-  }
+export default function Main() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <FiltersBar />
+      <RestaurantsMain />
+    </div>
+  );
 }
